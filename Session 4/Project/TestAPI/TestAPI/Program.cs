654a6691276333c41b5d96ac6a4f3baa5173f25f
Sina Sabzevari <sina.sabzevari@outlook.com>
2024-02-29 17:58:24 +0330

    add Session 5

---
+++ Session 4/Project/TestAPI/TestAPI/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+HttpClient client = new HttpClient();
+client.DefaultRequestHeaders.Add("ApiKey", "token");
+int survey_id = 1;
+var uri = $"https://survey.porsline.ir/api/v2/surveys/{survey_id}/";
+var response=await client.GetAsync(uri);
+var content=await response.Content.ReadAsStringAsync();
+
